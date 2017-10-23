b7256952b18d4b22ad07e1cee16d32f7358d06f3
khurramsaeed <5n8BX6dp>
2017-10-23 11:04:40 +0200

    OverActivity design finish with tablelayout (Khurram & Nikolaj)

--- app/src/main/java/com/company/sticksnsushi/activities/WelcomeActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/WelcomeActivity.java
@@ -1,15 +1,27 @@
 package com.company.sticksnsushi.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.company.sticksnsushi.R;
 
-public class WelcomeActivity extends BaseActivity {
+public class WelcomeActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedState) {
         super.onCreate(savedState);
 
         setContentView(R.layout.activity_welcome);
+
+        Button b = (Button) findViewById(R.id.overview_btn);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, OverviewActivity.class);
+        startActivity(i);
     }
 }
