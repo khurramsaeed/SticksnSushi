63c924b4bcc8b420c3e7c8b611b12dfaf7030c26
khurramsaeed <5n8BX6dp>
2017-10-28 21:07:26 +0200

    SlideAnimations added in styles, anim resource folder, AppManifest updated

--- app/src/main/java/com/company/sticksnsushi/activities/OverviewActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/OverviewActivity.java
@@ -11,23 +11,13 @@ import com.company.sticksnsushi.R;
  * Created by Khurram Saeed Malik on 23/10/2017.
  */
 
-public class OverviewActivity extends BaseActivity implements View.OnClickListener {
-    View target;
+public class OverviewActivity extends BaseActivity {
+
     @Override
     public void onCreate(Bundle savedState) {
         super.onCreate(savedState);
-
         setContentView(R.layout.activity_overview);
-
-        target = (View) findViewById(R.id.target_activity_overview);
-        target.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view == target) {
-            Intent intent =  new Intent(this, MenuOverviewActivity.class);
-            startActivity(intent);
-        }
-    }
+    public void intentMenu(View view) { startActivity(new Intent(this, MenuOverviewActivity.class));    }
 }
