dc797c8ea2db829296de23469d4dbf80869deb15
Swagam <s165155@student.dtu.dk>
2017-11-02 14:32:17 +0100

    Design of Cart started.

--- app/src/main/java/com/company/sticksnsushi/activities/MainActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
