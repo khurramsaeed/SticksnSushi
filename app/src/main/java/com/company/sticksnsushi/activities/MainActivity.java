81131c63b8e8997588d857f49b1f822360d5647a
khurramsaeed <5n8BX6dp>
2017-11-02 11:52:36 +0100

    NavDrawer items change screen with fragments now. Default screen is initially set to item1

--- app/src/main/java/com/company/sticksnsushi/activities/MainActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/MainActivity.java
@@ -2,9 +2,9 @@ package com.company.sticksnsushi.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +13,9 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.company.sticksnsushi.R;
+import com.company.sticksnsushi.fragments.Item1;
+import com.company.sticksnsushi.fragments.Item2;
+import com.company.sticksnsushi.fragments.Item3;
 
 /**
  * Created by Khurram Saeed Malik on 09/10/2017.
@@ -25,14 +28,8 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Skærmen man starter i:
+        displaySelectedItem(R.id.item_1);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,23 +80,34 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        displaySelectedItem(id);
 
-        } else if (id == R.id.nav_slideshow) {
+        return true;
+    }
 
-        } else if (id == R.id.nav_manage) {
+    /**
+     * Handles logic of changning fragment while selecting Item from NavDrawer
+     * @param id
+     */
+    private void displaySelectedItem(int id) {
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_share) {
+        switch (id) {
+            case R.id.item_1: fragment = new Item1();
+                break;
+            case R.id.item_2: fragment = new Item2();
+                break;
+            case R.id.item_3: fragment = new Item3();
+                break;
+        }
 
-        } else if (id == R.id.nav_send) {
-
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.activity_main_content, fragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     public void goToWelcome(View view) { startActivity(new Intent(this, WelcomeActivity.class));
