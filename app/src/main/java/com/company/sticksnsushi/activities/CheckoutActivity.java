8744c19948213a84addf3229dc1fbd2d16751097
khurramsaeed <5n8BX6dp>
2017-11-20 20:17:57 +0100

    Icons for Checkout Navigation added

--- app/src/main/java/com/company/sticksnsushi/activities/CheckoutActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/CheckoutActivity.java
@@ -4,9 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.company.sticksnsushi.R;
