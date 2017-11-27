ef1f66e64215d025037436fd10e22d4b7ba4c119
Swagam <s165155@student.dtu.dk>
2017-11-27 12:31:04 +0100

    User Information fragment added with minor changes and fixes.

--- app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
+++ app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
@@ -1,10 +1,31 @@
 package com.company.sticksnsushi.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * Created by Khurram Saeed Malik on 20/11/2017.
- */
+import com.company.sticksnsushi.R;
+
 
 public class InformationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_information, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Information");
+
+    }
 }
+
