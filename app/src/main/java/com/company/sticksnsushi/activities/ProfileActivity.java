f33fff6bd8c873c4d098daabf69ed3a9779ff069
Swagam <s165155@student.dtu.dk>
2017-11-29 22:18:54 +0100

    Changes in User Profile: Added Fragments.

--- app/src/main/java/com/company/sticksnsushi/activities/ProfileActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/ProfileActivity.java
@@ -1,9 +1,22 @@
 package com.company.sticksnsushi.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 
 import com.company.sticksnsushi.R;
 import com.company.sticksnsushi.activities.BaseActivity;
+import com.company.sticksnsushi.fragments.AllergiesFragment;
+import com.company.sticksnsushi.fragments.CartFragment;
+import com.company.sticksnsushi.fragments.PreviousOrders;
+import com.company.sticksnsushi.fragments.TakeAwayFragment;
+import com.company.sticksnsushi.fragments.UserProfileFragment1;
+import com.company.sticksnsushi.fragments.UserProfileFragment2;
+import com.company.sticksnsushi.fragments.UserProfileFragment3;
+import com.company.sticksnsushi.fragments.UserProfileFragment4;
 
 /**
  * Created by Swagam on 20/11/2017.
@@ -18,5 +31,31 @@ public class ProfileActivity extends BaseActivity {
         super.onCreate(savedState);
 
         setContentView(R.layout.activity_profile);
+
+        displaySelectedItem(1);
+    }
+
+    private void displaySelectedItem(int id) {
+        Fragment fragment = null;
+
+        switch (id) {
+            case R.id.editInfo: fragment = new UserProfileFragment1();
+                break;
+            case R.id.editFav: fragment = new UserProfileFragment2();
+                break;
+            case R.id.editAllergies: fragment = new UserProfileFragment3();
+                break;
+
+            case R.id.editRestaurant: fragment = new UserProfileFragment4();
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.activity_main_content, fragment).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
---
+++ app/src/main/java/com/company/sticksnsushi/fragments/UserProfileFragment3.java
@@ -0,0 +1,24 @@
+package com.company.sticksnsushi.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.company.sticksnsushi.R;
+
+/**
+ * Created by Swagam on 29/11/2017.
+ */
+
+public class UserProfileFragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_userprofile3, container, false);
+    }
+
+}
