67066a360328ad9b8e27e94b3741407b1bcf18d2
khurramsaeed <5n8BX6dp>
2018-01-03 12:44:43 +0100

    Fixed Screen Rotation in NavDrawerActivity and Child Fragments to it, sidebar cart updated

--- app/src/main/java/com/company/sticksnsushi/activities/NavDrawerActivity.java
+++ app/src/main/java/com/company/sticksnsushi/activities/NavDrawerActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -24,6 +25,16 @@ import com.company.sticksnsushi.fragments.AllergiesFragment;
 
 public class NavDrawerActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
+    private static final String TAG = "NavDrawerActivity";
+
+    private static final String BUNDLE_STATE = "BUNDLE_STATE";
+    // Conceptuel two-states
+    private static final int STATE_VIEWING = 1;
+    private static final int STATE_EDITING = 2;
+
+    private int currentState;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +47,6 @@ public class NavDrawerActivity extends BaseActivity implements NavigationView.On
         if (getSupportActionBar() != null) {
             toolbar.setTitle("Takeaway");
         }
-        // Default set to: Udforsk menu
-        displaySelectedItem(R.id.item_takeaway);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -47,7 +56,52 @@ public class NavDrawerActivity extends BaseActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Screen rotation: Editing fields fix
+        if (savedInstanceState == null) {
+            Log.d(TAG, "onCreate: STATE_VIEWING");
+
+            // Default set to: Udforsk menu
+            displaySelectedItem(R.id.item_takeaway);
+
+            changeState(STATE_VIEWING);
+        } else {
+            Log.d(TAG, "onCreate: BUNDLE_STATE");
+
+
+            changeState(savedInstanceState.getInt(BUNDLE_STATE));
+        }
     }
+
+    /**
+     * This method is called when you rotate screen
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Saved state is reloaded here
+        outState.putInt(BUNDLE_STATE, currentState);
+    }
+
+    private void changeState(int state) {
+        if (state == currentState)
+            return;
+
+        currentState = state;
+
+        if (state == STATE_VIEWING) {
+
+
+        } else if (state == STATE_EDITING) {
+
+        } else
+            throw new IllegalArgumentException("Invalid state: " + state);
+
+    }
+
 
     @Override
     public void onBackPressed() {
