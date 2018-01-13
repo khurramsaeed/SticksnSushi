4a2b64739f2278cd8324c02469082540c471994a
Swagam <s165155@student.dtu.dk>
2018-01-13 15:12:16 +0100

    Added verification to InformationFragment.

--- app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
+++ app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
@@ -7,14 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.company.sticksnsushi.R;
+import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.Step;
+import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 
 
-public class InformationFragment extends Fragment implements Step {
+public class InformationFragment extends Fragment implements BlockingStep {
+
+    EditText fullName, phone, adress, postalnr, city;
 
     @Nullable
     @Override
@@ -27,13 +33,22 @@ public class InformationFragment extends Fragment implements Step {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        fullName = (EditText) view.findViewById(R.id.editTextName);
+        phone = (EditText) view.findViewById(R.id.editTextPhone);
+        adress = (EditText) view.findViewById(R.id.editTextAdress);
+        postalnr = (EditText) view.findViewById(R.id.editTextPostalAdress);
+        city = (EditText) view.findViewById(R.id.editTextCity);
+
         getActivity().setTitle("Bestilling");
 
     }
 
+
+
     @Nullable
     @Override
     public VerificationError verifyStep() {
+
         return null;
     }
 
@@ -44,6 +59,52 @@ public class InformationFragment extends Fragment implements Step {
 
     @Override
     public void onError(@NonNull VerificationError error) {
+
+
+    }
+
+    @Override
+    public void onNextClicked(StepperLayout.OnNextClickedCallback callback) {
+
+        if (fullName.getText().toString().trim().length() == 0) {
+            fullName.setError("Du skal indtaste dit navn");
+        } else {
+            callback.goToNextStep();
+        }
+
+        if (phone.getText().toString().trim().length() == 0) {
+            phone.setError("Du skal indtaste dit telefonnummer");
+        } else {
+            callback.goToNextStep();
+        }
+
+        if (adress.getText().toString().trim().length() == 0) {
+            adress.setError("Du skal indtaste din adresse");
+        } else {
+            callback.goToNextStep();
+        }
+
+        if (postalnr.getText().toString().trim().length() == 0) {
+            postalnr.setError("Du skal indtaste dit post nummer");
+        } else {
+            callback.goToNextStep();
+        }
+
+        if (city.getText().toString().trim().length() == 0) {
+            city.setError("Du skal indtaste din by");
+        } else {
+            callback.goToNextStep();
+        }
+
+    }
+
+    @Override
+    public void onCompleteClicked(StepperLayout.OnCompleteClickedCallback callback) {
+
+    }
+
+    @Override
+    public void onBackClicked(StepperLayout.OnBackClickedCallback callback) {
 
     }
 }
