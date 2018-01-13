f7e85e850fcc22cf87da75eb616636a98ea9bae3
Swagam <s165155@student.dtu.dk>
2018-01-13 16:49:52 +0100

    Added verification to InformationFragment.

--- app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
+++ app/src/main/java/com/company/sticksnsushi/fragments/InformationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,7 @@ import com.stepstone.stepper.VerificationError;
 
 public class InformationFragment extends Fragment implements BlockingStep {
 
-    EditText fullName, phone, adress, postalnr, city;
+    EditText editFullName, editPhone, editAdress, editPostalnr, editCity;
 
     @Nullable
     @Override
@@ -33,17 +34,15 @@ public class InformationFragment extends Fragment implements BlockingStep {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        fullName = (EditText) view.findViewById(R.id.editTextName);
-        phone = (EditText) view.findViewById(R.id.editTextPhone);
-        adress = (EditText) view.findViewById(R.id.editTextAdress);
-        postalnr = (EditText) view.findViewById(R.id.editTextPostalAdress);
-        city = (EditText) view.findViewById(R.id.editTextCity);
+        editFullName = (EditText) view.findViewById(R.id.editTextName);
+        editPhone = (EditText) view.findViewById(R.id.editTextPhone);
+        editAdress = (EditText) view.findViewById(R.id.editTextAdress);
+        editPostalnr = (EditText) view.findViewById(R.id.editTextPostalAdress);
+        editCity = (EditText) view.findViewById(R.id.editTextCity);
 
         getActivity().setTitle("Bestilling");
 
     }
-
-
 
     @Nullable
     @Override
@@ -66,35 +65,42 @@ public class InformationFragment extends Fragment implements BlockingStep {
     @Override
     public void onNextClicked(StepperLayout.OnNextClickedCallback callback) {
 
-        if (fullName.getText().toString().trim().length() == 0) {
-            fullName.setError("Du skal indtaste dit navn");
-        } else {
-            callback.goToNextStep();
+        final String fullName = editFullName.getText().toString().trim();
+        final String phone = editPhone.getText().toString().trim();
+        final String adress = editAdress.getText().toString().trim();
+        final String postalnr = editPostalnr.getText().toString().trim();
+        final String city = editCity.getText().toString().trim();
+
+        if (TextUtils.isEmpty(fullName) ) {
+            editFullName.setError("Du skal indtaste dit fulde navn");
+            editFullName.requestFocus();
+            return;
         }
 
-        if (phone.getText().toString().trim().length() == 0) {
-            phone.setError("Du skal indtaste dit telefonnummer");
-        } else {
-            callback.goToNextStep();
+        if(TextUtils.isEmpty(phone)){
+            editPhone.setError("Du skal indtaste dit telefon nummer");
+            editPhone.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(adress)) {
+            editAdress.setError("Du skal indtaste din adresse");
+            editAdress.requestFocus();
+            return;
         }
 
-        if (adress.getText().toString().trim().length() == 0) {
-            adress.setError("Du skal indtaste din adresse");
-        } else {
-            callback.goToNextStep();
+        if (TextUtils.isEmpty(postalnr)) {
+            editPostalnr.setError("Du skal indtaste dit post nummer");
+            editPostalnr.requestFocus();
+            return;
         }
 
-        if (postalnr.getText().toString().trim().length() == 0) {
-            postalnr.setError("Du skal indtaste dit post nummer");
-        } else {
-            callback.goToNextStep();
+        if (TextUtils.isEmpty(city)) {
+            editCity.setError("Du skal indtaste din by");
+            editCity.requestFocus();
+            return;
         }
 
-        if (city.getText().toString().trim().length() == 0) {
-            city.setError("Du skal indtaste din by");
-        } else {
-            callback.goToNextStep();
-        }
+        callback.goToNextStep();
 
     }
 
